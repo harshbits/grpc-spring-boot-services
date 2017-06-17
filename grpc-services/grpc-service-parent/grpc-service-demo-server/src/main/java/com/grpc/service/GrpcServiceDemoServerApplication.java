b856f2c70799e610fef8c6fc10052613bc272140
harshbhavsar <harsh.bhavsar@copart.com>
2017-06-17 17:39:27 -0500

    sever client app connected with eureka

--- grpc-services/grpc-service-parent/grpc-service-demo-server/src/main/java/com/grpc/service/GrpcServiceDemoServerApplication.java
+++ grpc-services/grpc-service-parent/grpc-service-demo-server/src/main/java/com/grpc/service/GrpcServiceDemoServerApplication.java
@@ -3,8 +3,10 @@ package com.grpc.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableAutoConfiguration
 public class GrpcServiceDemoServerApplication {
 
--- grpc-services/grpc-service-parent/grpc-service-demo-server/src/main/java/com/grpc/service/services/GreetingService.java
+++ grpc-services/grpc-service-parent/grpc-service-demo-server/src/main/java/com/grpc/service/services/GreetingService.java
@@ -1,17 +1,21 @@
 package com.grpc.service.services;
 
-import com.copart.grpc.service.gen.GreeterGrpc;
-import com.copart.grpc.service.gen.GreeterOuterClass;
+import com.grpc.service.gen.GreeterGrpc;
+import com.grpc.service.gen.GreeterOuterClass;
 import com.grpc.service.support.GrpcService;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 
 @GrpcService(interceptors = { LogInterceptor.class })
+@Slf4j
 public class GreetingService extends GreeterGrpc.GreeterImplBase {
 	
 	@Override
 	public void sayHello(GreeterOuterClass.HelloRequest request,
 			StreamObserver<GreeterOuterClass.HelloReply> responseObserver) {
+		
+		log.info(request.getName());
 		final GreeterOuterClass.HelloReply.Builder replyBuilder = GreeterOuterClass.HelloReply.newBuilder()
 				.setMessage("Hello " + request.getName());
 		responseObserver.onNext(replyBuilder.build());
